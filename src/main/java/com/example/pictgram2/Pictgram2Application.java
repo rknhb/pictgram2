3f28acbede51c855846430e1067239500fd515d4
rknhb <rakizunanohachan@gmail.com>
2023-04-18 14:46:36 +0900

    first commit

---
+++ src/main/java/com/example/pictgram2/Pictgram2Application.java
@@ -0,0 +1,13 @@
+package com.example.pictgram2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pictgram2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pictgram2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pictgram2/Pictgram2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pictgram2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pictgram2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
